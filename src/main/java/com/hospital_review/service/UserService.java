b8824987b10c94c911a380b589306b87dc081334
lay423 <junpang486@gmail.com>
2022-11-28 14:52:17 +0900

    [add] errorcode enum 추가, RestControllerAdvice 적용

--- src/main/java/com/hospital_review/service/UserService.java
+++ src/main/java/com/hospital_review/service/UserService.java
@@ -3,6 +3,8 @@ package com.hospital_review.service;
 import com.hospital_review.domain.User;
 import com.hospital_review.domain.dto.UserDto;
 import com.hospital_review.domain.dto.UserJoinRequest;
+import com.hospital_review.exceptionManager.ErrorCode;
+import com.hospital_review.exceptionManager.HospitalReviewAppException;
 import com.hospital_review.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,9 @@ public class UserService {
     public UserDto join(UserJoinRequest request) {
 
         userRepository.findByUserName(request.getUserName())
-                .ifPresent(user -> new RuntimeException("해당 UserName이 중복 됩니다."));
+                .ifPresent(user -> {
+                    throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("UserName:%s", request.getUserName()));
+                });
         User savedUser = userRepository.save(request.toEntity());
 
         return UserDto.builder()
