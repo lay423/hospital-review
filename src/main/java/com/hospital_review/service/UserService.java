026e7b763393668846ba9813db4529fc70f2ef0a
lay423 <junpang486@gmail.com>
2022-11-29 13:31:52 +0900

    [update] test코드 리팩토링

--- src/main/java/com/hospital_review/service/UserService.java
+++ src/main/java/com/hospital_review/service/UserService.java
@@ -18,13 +18,17 @@ public class UserService {
     private final BCryptPasswordEncoder encoder;
 
     public UserDto join(UserJoinRequest request) {
-
+        // 비즈니스 로직 - 회원 가입
+        // 회원 userName(id) 중복 Check
+        // 중복이면 회원가입 x --> Exception(예외)발생
+        // 있으면 에러처리
         userRepository.findByUserName(request.getUserName())
-                .ifPresent(user -> {
+                .ifPresent(user ->{
                     throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("UserName:%s", request.getUserName()));
                 });
-        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
 
+        // 회원가입 .save()
+        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
         return UserDto.builder()
                 .id(savedUser.getId())
                 .userName(savedUser.getUserName())
--- src/test/java/com/hospital_review/controller/UserControllerTest.java
+++ src/test/java/com/hospital_review/controller/UserControllerTest.java
@@ -9,18 +9,22 @@ import com.hospital_review.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(UserController.class)
 class UserControllerTest {
 
     @Autowired
@@ -34,16 +38,19 @@ class UserControllerTest {
 
     @Test
     @DisplayName("회원가입 성공")
+    @WithMockUser
     void join_success() throws Exception {
         UserJoinRequest userJoinRequest = UserJoinRequest.builder()
-                .userName("junha")
-                .password("1234")
-                .email("junha@gamil.com")
+                .userName("kyeongrok")
+                .password("1q2w3e4r")
+                .email("oceanfog1@gmail.com")
                 .build();
 
-        when(userService.join(any())).thenReturn(mock(UserDto.class));
 
+
+        when(userService.join(any())).thenReturn(mock(UserDto.class));
         mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
@@ -51,6 +58,7 @@ class UserControllerTest {
     }
 
     @Test
+    @WithMockUser
     @DisplayName("회원가입 실패")
     void join_fail() throws Exception{
         UserJoinRequest userJoinRequest = UserJoinRequest.builder()
@@ -62,6 +70,7 @@ class UserControllerTest {
         when(userService.join(any())).thenThrow(new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
 
         mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
