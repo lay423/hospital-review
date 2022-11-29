0caaba241db60c537a4c2cfe68598d18bbc797ff
lay423 <junpang486@gmail.com>
2022-11-29 13:02:17 +0900

    [add] password 암호화해서 저장

--- src/main/java/com/hospital_review/service/UserService.java
+++ src/main/java/com/hospital_review/service/UserService.java
@@ -7,6 +7,7 @@ import com.hospital_review.exceptionManager.ErrorCode;
 import com.hospital_review.exceptionManager.HospitalReviewAppException;
 import com.hospital_review.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
     public UserDto join(UserJoinRequest request) {
 
@@ -21,7 +23,7 @@ public class UserService {
                 .ifPresent(user -> {
                     throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("UserName:%s", request.getUserName()));
                 });
-        User savedUser = userRepository.save(request.toEntity());
+        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
 
         return UserDto.builder()
                 .id(savedUser.getId())
