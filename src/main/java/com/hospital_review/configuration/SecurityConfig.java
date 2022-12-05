4b761c47fcb0561650869bc2cc52083c2c2111c6
lay423 <junpang486@gmail.com>
2022-12-05 11:28:08 +0900

    [add] ReviewController, post요청 막기

--- src/main/java/com/hospital_review/configuration/SecurityConfig.java
+++ src/main/java/com/hospital_review/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hospital_review.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
---
+++ src/main/java/com/hospital_review/domain/dto/ReviewCreateRequest.java
@@ -0,0 +1,12 @@
+package com.hospital_review.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ReviewCreateRequest {
+    private int id;
+}
