372073681f7037f4ac00a9a8cebafc1771a6478a
lay423 <junpang486@gmail.com>
2022-12-05 14:02:40 +0900

    [update] Token없으면 접근 거부하기

--- src/main/java/com/hospital_review/configuration/JwtTokenFilter.java
+++ src/main/java/com/hospital_review/configuration/JwtTokenFilter.java
@@ -1,7 +1,12 @@
 package com.hospital_review.configuration;
 
 import com.hospital_review.service.UserService;
+import com.hospital_review.utils.JwtTokenUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,16 +19,33 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final UserService userService;
     private final String secretKey;
 
+    // Key로 token을 열어서 Claims꺼내기
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader={}", authorizationHeader);
+        String token;
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) {
+            log.error("token 추출에 실패 했습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        JwtTokenUtil.isExpired(token, secretKey);
+
         //권한 부여 역할
         //현재는 모두 닫혀 있습니다.
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER"))    );
--- src/main/java/com/hospital_review/utils/JwtTokenUtil.java
+++ src/main/java/com/hospital_review/utils/JwtTokenUtil.java
@@ -7,6 +7,17 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+    private static Claims extractClaims(String token, String key) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+    public static String getUserName(String token, String secretKey) {
+        return extractClaims(token, secretKey).get("userName", String.class);
+    }
+
+    public static boolean isExpired(String token, String key) {
+        Date expiredDate = extractClaims(token, key).getExpiration(); // expire timestamp를 return함
+        return expiredDate.before(new Date()); // 현재보다 전인지 check를 합니다.
+    }
     public static String createToken(String userName, String key, long expiredTimeMs) {
         Claims claims = Jwts.claims();
         claims.put("userName", userName);
